889713b5f443a0919a9af44812b3172a02e1f44d
CGhosh81 <chayan07ghosh@gmail.com>
2025-01-17 10:56:50 +0530

    update 1

--- project2.0/signuppage/signuppage.js
+++ project2.0/signuppage/signuppage.js
@@ -1,26 +1,21 @@
-function valid(){
+function valid() {
     var pass = document.getElementById("password").value;
     var conpass = document.getElementById("confirm-password").value;
-    var message="";
-    if(pass !== conpass){
-        message="Password Not match";
-        
+    var message = "";
+    if (pass !== conpass) {
+      message = "Password does not match";
+    } else if (pass.length < 8) {
+      message = "Enter a password with a minimum of 8 characters";
+    } else {
+      var upp = /[a-z]/.test(pass);
+      var low = /[A-Z]/.test(pass);
+      var digit = /[0-9]/.test(pass);
+      var spec = /[@!#$%^&*~.;:]/.test(pass);
+      if (!upp || !low || !digit || !spec) {
+        message = "Password must include uppercase, lowercase, digit, and special character";
+      } else {
+        message = "Password is valid";
+      }
     }
-    else if(pass.length<8){
-        message="enter password minimum 8 character";
-        
-    }
-    else{
-        var upp=/[a-z]/.test(pass);
-        var low=/[A-Z]/.test(pass);
-        var digit=/[0-9]/.test(pass);
-        var spec=/[@!#$%^&*~.;:]/.test(pass);
-        if( !upp  || !low || !digit || !spec ){
-         message="Enter password mixed like digit upper leter lower letter specil character";
-        }
-        else{
-            message="password is valid";
-        }
-    }
-   alert(message)
-    }
+    alert(message);
+  }
